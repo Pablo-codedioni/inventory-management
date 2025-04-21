f86152b3a3b0f5856473397594dc3c817ec7fcd5
Pablo Dionisio <uriel-nike@outlook.com>
2025-04-21 16:51:42 -0600

    index.ts -log

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,8 +29,14 @@ app.use("/products", productRoutes); // http://localhost:8000/products
 app.use("/users", userRoutes); // http://localhost:8000/users
 app.use("/expenses", expenseRoutes); // http://localhost:8000/expenses
 
-/* SERVER */
+
 const port = Number(process.env.PORT) || 3001;
+console.log("✅ Starting server...");
+console.log("PORT from env:", process.env.PORT);
+console.log("Resolved port:", port);
+
+/* SERVER */
+
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
